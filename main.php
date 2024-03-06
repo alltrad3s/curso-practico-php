72c190692fd4dcce44f9506bb83deb42b80feb56
Jorge Diaz <me@jorgediaz.pw>
2024-03-06 10:02:57 -0600

    Juego PHP

--- main.php
+++ main.php
@@ -1,2 +1,20 @@
 <?php
-print_r($_REQUEST);
+// Linea va con index.html
+//print_r($_REQUEST);
+
+$palabras = array("sol", "luna", "cielo", "luz", "estrellas", "lluvia");
+$form = "<form action='analisis.php'>";
+
+//Se elimina la siguiente linea
+//$palabraDesordenadas = array();
+
+for($i = 0; $i < count($palabras); $i++) {
+    $form .= "La palabra: " .str_shuffle($palabras[$i]). " " . 
+    "<input type='text' name='palabra".$i."'>". 
+    "<br>";
+}
+
+$button = "<button type='submit'>Enviar</button>";
+$formCierre = "</form>";
+
+echo $form.$button.$formCierre;
