07b481b223aa56890b6f970109febe8090ad4b5a
Jorge Diaz <me@jorgediaz.pw>
2024-03-07 14:42:39 -0600

    Reto 3

--- reto3-descuentos.php
+++ reto3-descuentos.php
@@ -14,3 +14,24 @@ function calcularDescuento($precioOriginal) {
 
 // Ejemplo de uso
 calcularDescuento(100); // Suponiendo que el precio original del producto es $100
+
+/* EJEMPLO CON RETURN
+function calcularDescuento($precioOriginal) {
+    $valorDescuento = $precioOriginal * 0.35;
+    $nuevoPrecio = $precioOriginal - $valorDescuento;
+    
+    // Retorna un array asociativo con el precio original y el nuevo precio
+    return [
+        'precioOriginal' => $precioOriginal,
+        'nuevoPrecio' => $nuevoPrecio
+    ];
+}
+
+// Llamada a la función y almacenamiento de los valores retornados
+$resultado = calcularDescuento(100);
+
+// Ahora puedes decidir qué hacer con los valores retornados, por ejemplo, imprimirlos
+echo "Precio Original: $" . number_format($resultado['precioOriginal'], 2) . "\n";
+echo "Nuevo Precio con Descuento: $" . number_format($resultado['nuevoPrecio'], 2) . "\n";
+
+*/
